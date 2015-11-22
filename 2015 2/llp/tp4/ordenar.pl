1199b0faabcde6ce891a5165def9a4ce09fa3086
Saulo Antunes <sauloantuness@gmail.com>
2015-11-22 20:23:10 -0200

    ordenar

--- 2015 2/llp/tp4/ordenar.pl
+++ 2015 2/llp/tp4/ordenar.pl
@@ -1,3 +1,22 @@
-# Exercício 15
+# Exercício 15 (bubble sort)
 
-ordenar.pl
+tamanho([], 0).
+tamanho([X|Y], N1)
+	:- tamanho(Y,N), N1 is N + 1.
+
+concatena([],L,L).
+concatena([X|Y],L,[X|L1])
+	:- concatena(Y,L,L1).
+
+troca([X],[X]).
+troca([X,Y|Z],W)
+	:- X =< Y, troca([Y|Z],W1), concatena([X], W1, W).
+troca([X,Y|Z],W)
+	:- X > Y, troca([X|Z],W1), concatena([Y], W1, W).
+
+trocan(0,L,L).
+trocan(N1,L,L1)
+	:- troca(L, L2), trocan(N,L2,L1), N1 is N+1.
+
+ordenar(L,L1)
+	:- tamanho(L,N), trocan(N,L,L1).
